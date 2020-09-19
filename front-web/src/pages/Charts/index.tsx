150cb9f4a367a18fe321b03f73d0fa773b0c805f
eliseusbrito <eliseusbrito@gmail.com>
2020-09-19 14:54:13 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -22,7 +22,8 @@ const initialPieData = {
     series:[]
 }
 
-const BASE_URL = 'http://localhost:8080'
+// const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1pesquisagames.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
